88ffa46596d5c8d8e18bf2b35518d7accecf7e4f
J. Bergmann <bergmannj@posteo.de>
2024-05-05 11:28:42 +0200

    chore: fix lakefile

--- lakefile.lean
+++ lakefile.lean
@@ -5,26 +5,10 @@ package time {
   precompileModules := if get_config? env = some "noprecompile" then false else true
 }
 
-require std from git "https://github.com/leanprover/std4" @ "v4.8.0-rc1"
-
-meta if get_config? env = some "dev" then
-require CMark from git
-  "https://github.com/xubaiw/CMark.lean" @ "main"
-
-meta if get_config? env = some "dev" then
-require «UnicodeBasic» from git
-  "https://github.com/fgdorais/lean4-unicode-basic" @ "main"
-
-meta if get_config? env = some "dev" then
-require Cli from git
-  "https://github.com/mhuisi/lean4-cli" @ "nightly"
-
-meta if get_config? env = some "dev" then
-require leanInk from git
-  "https://github.com/hargonix/LeanInk" @ "doc-gen"
-
 meta if get_config? env = some "dev" then
 require «doc-gen4» from  git "https://github.com/leanprover/doc-gen4" @ "main"
+
+require std from git "https://github.com/leanprover/std4" @ "v4.8.0-rc1"
 
 @[default_target]
 lean_lib Time
