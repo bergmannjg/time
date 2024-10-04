937184d198e5ff1f90cd912dc4cbc57ea2032af1
J. Bergmann <bergmannj@posteo.de>
2024-10-04 09:22:38 +0200

    chore: update to v4.11

--- lakefile.lean
+++ lakefile.lean
@@ -8,7 +8,7 @@ package time {
 meta if get_config? env = some "dev" then
 require «doc-gen4» from  git "https://github.com/leanprover/doc-gen4" @ "v4.11.0-rc1"
 
-require batteries from git "https://github.com/leanprover-community/batteries" @ "v4.11.0-rc1"
+require batteries from git "https://github.com/leanprover-community/batteries" @ "v4.11.0"
 
 @[default_target]
 lean_lib Time
